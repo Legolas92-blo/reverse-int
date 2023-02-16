c9ce5f8fd467fdd2a951de2a16bcf28aac581f7d
Asya Beladidze <asya2100@gmail.com>
2023-02-16 23:07:39 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	if (n > 0) {
+		return n.toString().split('').reverse().join('')
+	} else {
+		const nNegative = n * (-1);
+		return nNegative.toString().split('').reverse().join('')
+	}
+
 }
